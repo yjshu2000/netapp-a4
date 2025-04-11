c0504794074f8f7aecac1e3ba09c694793a29e52
Jacen <yjshu2017@gmail.com>
2025-04-11 19:02:16 -0400

    add card to posts

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -28,7 +28,24 @@ $.ajax({
             spinnerBox.classList.add('not-visible');
             console.log(data);
             data.forEach(el => {
-                postsBox.innerHTML += `${el.title} - <b>${el.body}</b><br>`
+                postsBox.innerHTML += `
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">${el.title}</h5>
+                        <p class="card-text">${el.body}</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-2">
+                                <a href="#" class="btn btn-primary">Details</a>
+                            </div>
+                            <div class="col-2">
+                                <a href="#" class="btn btn-primary">Like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `
             }, 1000);
         }
 )

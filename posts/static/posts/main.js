e3f2184e51540a2a7767ed23076fb30b4df3dc83
Jacen <yjshu2017@gmail.com>
2025-04-11 18:23:06 -0400

    ajax call with dom manipulation (Hallo Welt)

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -3,4 +3,16 @@ console.log("Hello from main.js");
 const helloWorldBox = document.getElementById("hello-world");
 //he put helloWolrdBox :( sad
 
-helloWorldBox.innerHTML = "Hello World!";
+//helloWorldBox.innerHTML = "Hello World!";
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world',
+    success: function(response) {
+        console.log("Success:", response.text);
+        helloWorldBox.innerHTML = response.text;
+    },
+    error: function(xhr, status, error) {
+        console.error("Error:", error);
+    }
+})
--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
+    hello_world_view,
 )
 
 app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name='main_board'),
+    path('hello-world/', hello_world_view, name='hello-world'),
 ]

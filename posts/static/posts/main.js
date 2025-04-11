c4ef47def50874f9e58304b1168f7da7d5be90ae
Jacen <yjshu2017@gmail.com>
2025-04-11 18:44:52 -0400

    updated new way to show posts in main??

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -2,8 +2,8 @@ console.log("Hello from main.js");
 
 const helloWorldBox = document.getElementById("hello-world");
 //he put helloWolrdBox :( sad
-
 //helloWorldBox.innerHTML = "Hello World!";
+const postsBox = document.getElementById("posts-box");
 
 $.ajax({
     type: 'GET',
@@ -13,6 +13,22 @@ $.ajax({
         helloWorldBox.innerHTML = response.text;
     },
     error: function(xhr, status, error) {
+        console.error("Error:", error);
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response) {
+        console.log("Success:", response.data);
+        const data = response.data;
+        console.log(data);
+        data.forEach(el => {
+            postsBox.innerHTML += `${el.title} - <b>${el.body}</b><br>`
+        });
+    },
+    error: function(error) {
         console.error("Error:", error);
     }
 })
--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
+    load_post_data_view,
     hello_world_view,
 )
 
 app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name='main_board'),
+    path('data/', load_post_data_view, name='posts_data'),
     path('hello-world/', hello_world_view, name='hello-world'),
 ]

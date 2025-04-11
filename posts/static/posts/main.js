aa24f3ae37117d96c7dc22c62bbd304c8becca92
Jacen <yjshu2017@gmail.com>
2025-04-11 18:53:22 -0400

    added a spinner

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -4,6 +4,7 @@ const helloWorldBox = document.getElementById("hello-world");
 //he put helloWolrdBox :( sad
 //helloWorldBox.innerHTML = "Hello World!";
 const postsBox = document.getElementById("posts-box");
+const spinnerBox = document.getElementById("spinner-box");
 
 $.ajax({
     type: 'GET',
@@ -23,10 +24,14 @@ $.ajax({
     success: function(response) {
         console.log("Success:", response.data);
         const data = response.data;
-        console.log(data);
-        data.forEach(el => {
-            postsBox.innerHTML += `${el.title} - <b>${el.body}</b><br>`
-        });
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible');
+            console.log(data);
+            data.forEach(el => {
+                postsBox.innerHTML += `${el.title} - <b>${el.body}</b><br>`
+            }, 1000);
+        }
+)
     },
     error: function(error) {
         console.error("Error:", error);

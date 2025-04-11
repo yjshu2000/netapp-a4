b86b7738484226780171420e7165236389f2ed59
Jacen <yjshu2017@gmail.com>
2025-04-11 17:08:21 -0400

    added avatar.png; updated admin.py and models.py for posts and profiles; did the migrations thing

--- profiles/models.py
+++ profiles/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
-    avatar = models.ImageField(default = avatar.png, upload_to='avatars/')
+    avatar = models.ImageField(default = "avatar.png", upload_to='avatars')
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"profile of the user {self.user.username}"
+        return f"profile of {self.user.username}"
